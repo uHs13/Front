8ba884e74aa3386ee170fd14b909ad8b2b188879
uHs13 <heitorhenriquedias@gmail.com>
2019-03-05 12:16:23 -0300

    Racionais

--- HTML/Init/js/Basic.js
+++ HTML/Init/js/Basic.js
@@ -28,12 +28,20 @@ class Basic{
 
 	}
 
+	changeValue(){
+
+		document.querySelector("#result").innerHTML = document.querySelector("#value").value;
+		console.log(document.querySelector("#result"));
+		console.log(document.querySelector("#value"))
+
+	}
+
 	addEventListenerAll(element, events, func) {//Como o addEventListener do js só adiciona um evento por vez em um elemento é preciso criar uma função para melhorar essa questão.
 
 		events.split(" ").forEach(event => {//Passamos para a função o elemento onde vão ser adicionados os eventos, os eventos e a função que vai determinar o comportamento do objeto quando o evento for disparado  
 			//Cada um dos eventos passados na string de eventos é adicionado ao elemento;
 			element.addEventListener(event, func, false);
-		
+			
 		});
 
 	}
